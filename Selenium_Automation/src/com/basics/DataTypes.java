275d47411f11c298f354e9e022521037c498a88a
Mandula Naresh Babu <naresh2223@gmail.com>
2021-04-15 20:50:40 +0530

    First Commit
    15-Apr-2021: Naresh: Selenium Java Project

---
+++ Selenium_Automation/src/com/basics/DataTypes.java
@@ -0,0 +1,41 @@
+package com.basics;
+
+public class DataTypes {
+
+	// main method - Its an entry point for java application
+	// main method is being called by a component called - JVM (Java Virtual Machine)
+	public static void main(String[] args) {
+		//Shortcuts - hotkeys
+		// To comment one line use //
+		// To comment a block of code or lines use ctrl+shift+/
+		// sysout + ctrl + spacebar
+		System.out.println("Hello world..!");
+		System.out.println("Welcome to Java and Selenium Training");
+		
+		// Primitive Data types in Java --> byte, short, int, long, float, double, char, boolean
+		byte a = 126;
+		System.out.println("Byte value of variable a is : " + a);
+		short b = 31000;
+		System.out.println("Short value of variable b is : " + b);
+		int c = 345346;
+		System.out.println("Integer value of variable c is : " + c);
+		long d = 3464564;
+		System.out.println("Long value of variable d is : " + d);
+		float e = 133444.6f;
+		System.out.println("Float value of variable e is : " + e);
+		double f = 1234.345345;
+		System.out.println("Double value of variable f is : " + f);
+		char g = 'y';
+		System.out.println("Char value of variable g is : " + g);
+		boolean h = true;
+		System.out.println("Boolean value of variable h is : " + h);
+		
+		System.out.println("Minimum value of byte data type is : " + Byte.MIN_VALUE);
+		System.out.println("Maximum value of byte data type is : " + Byte.MAX_VALUE);
+		
+		System.out.println("Minimum value of short data type is : " + Short.MIN_VALUE);
+		System.out.println("Maximum value of short data type is : " + Short.MAX_VALUE);
+		
+	}
+
+}
---
+++ Selenium_Automation/src/com/basics/Shortcuts.java
@@ -0,0 +1,11 @@
+package com.basics;
+
+public class Shortcuts {
+
+	// main method - entry point for java program
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
